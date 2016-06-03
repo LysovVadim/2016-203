f038c1c8255c68c19d6bd68cbd1a74efd09a012c
LysovVadim <lisov.vad@gmail.com>
2016-06-03 16:41:49 +0300

    Update Lysov.cpp

--- Lysov.cpp
+++ Lysov.cpp
@@ -89,8 +89,9 @@ for (k=0;k<N;k++)
  * Метод квадратного корня (метод Холецкого)
  */
 void Lysov::lab3()
-{    int i,j,k;
-    double L[N][N]summ,summ2, y[N],s;
+{   
+  int i,j,k;
+  double L[N][N],summ,summ2, y[N],s;
   for (int i=0; i<N; i++)
     for (int j=0; j<N; j++)
      {L[i][j]=0;}
@@ -104,47 +105,47 @@ for(j=1;j<N;j++)
 //3
 for (i=1;i<N;i++)
     {
-        for (k=0;k<=i-1;k++)
-        {
-           summ=summ+L[i][k]*L[i][k];
-        }
-        L[i][i]=sqrt(A[i][i]-summ);
-        summ=0;
+      for (k=0;k<=i-1;k++)
+      {
+        summ=summ+L[i][k]*L[i][k];
+      }
+      L[i][i]=sqrt(A[i][i]-summ);
+      summ=0;
 //4
-         if(i!=N-1)
-    {
+      if(i!=N-1)
+      {
         for(j=i+1;j<=N;j++)
         {
-            for (k=0;k<=j-1;k++)
-            {
-                summ2=summ2+L[i][k]*L[j][k];
-            }
-            L[j][i]=(A[j][i]-summ2)/(L[i][i]);
-            summ2=0;
+          for (k=0;k<=j-1;k++)
+          {
+            summ2=summ2+L[i][k]*L[j][k];
+          }
+        L[j][i]=(A[j][i]-summ2)/(L[i][i]);
+        summ2=0;
         }
+      } 
     }
-}
-
-y[0]=b[0]/L[0][0];
-for (i=1;i<N;i++)
-{  for (k=0;k<=i-1;k++)
-        {
-           s=s+L[i][k]*y[k];
-        }
+  y[0]=b[0]/L[0][0];
+  for (i=1;i<N;i++)
+  {  
+    for (k=0;k<=i-1;k++)
+      {
+         s=s+L[i][k]*y[k];
+      }
     y[i]=(b[i]-s)/L[i][i];
-    s=0;
-}
+  s=0;
+  }
 
-x[N-1]=y[N-1]/L[N-1][N-1];
-for (i=N-1;i>=0;i--)
-{
-     for (k=i+1;k<N;k++)
-        {
-           s=s+L[k][i]*x[k];
-        }
+  x[N-1]=y[N-1]/L[N-1][N-1];
+  for (i=N-1;i>=0;i--)
+  {
+    for (k=i+1;k<N;k++)
+      {
+          s=s+L[k][i]*x[k];
+      }
     x[i]=(y[i]-s)/L[i][i];
     s=0;
-}
+  }
 }
 
 
@@ -154,7 +155,20 @@ for (i=N-1;i>=0;i--)
  */
 void Lysov::lab4()
 {
-
+      int i,j;
+    double c[N],z[N];
+  c[1]=-A[0][1]/A[0][0];
+z[1]=b[0]/A[0][0];
+ for (i=1;i<N-1;i++)
+    {
+    	c[i+1]=-A[i][i+1]/(A[i][i-1]*c[i]+A[i][i]);
+    	z[i+1]=(b[i]-A[i][i-1]*z[i])/(A[i][i-1]*c[i]+A[i][i]);
+    }
+	x[N-1]=(b[N-1]-A[N-1][N-2]*z[N-1])/(A[N-1][N-1]+A[N-1][N-2]*c[N-1]);
+		for(i=N-2;i>=0;i--)
+        {
+        	x[i]=c[i+1]*x[i+1]+z[i+1];
+        }
 }
 
 
